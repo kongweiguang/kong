fd62d4de757f4429e29e6a540d8e3a69c9d3b798
kongweiguang <kongweiguang@icourt.cc>
2024-01-19 18:21:15 +0800

    fix version

--- kong-json/src/main/java/io/github/kongweiguang/json/JsonAry.java
+++ kong-json/src/main/java/io/github/kongweiguang/json/JsonAry.java
@@ -3,6 +3,7 @@ package io.github.kongweiguang.json;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import java.util.Collection;
+import java.util.List;
 
 import static java.util.Optional.ofNullable;
 
@@ -21,30 +22,82 @@ public class JsonAry {
         this.node = node;
     }
 
+    /**
+     * 工厂方法，创建JsonAry
+     *
+     * @return {@link JsonAry}
+     */
     public static JsonAry of() {
         return new JsonAry();
     }
 
+    /**
+     * 工厂方法，创建JsonAry
+     *
+     * @param node {@link ArrayNode}
+     * @return {@link JsonAry}
+     */
     public static JsonAry of(ArrayNode node) {
         return new JsonAry(node);
     }
 
+    /**
+     * 添加一个字符串
+     *
+     * @param str 内容
+     * @return {@link JsonAry}
+     */
     public JsonAry add(String str) {
         node.add(str);
         return this;
     }
 
+    /**
+     * 添加一个对象
+     *
+     * @param obj 内容
+     * @return {@link JsonAry}
+     */
     public JsonAry add(Object obj) {
+        if (obj instanceof String) {
+            return add((String) obj);
+        }
+
+        if (obj instanceof Collection) {
+            return add(((Collection<?>) obj));
+        }
+
         node.add(Json.toNode(Json.toStr(obj)));
         return this;
     }
 
+    /**
+     * 添加一个集合
+     *
+     * @param coll 集合
+     * @return {@link JsonAry}
+     */
     public JsonAry add(Collection<?> coll) {
         ofNullable(coll).ifPresent(e -> e.forEach(this::add));
         return this;
     }
 
+    /**
+     * 构建成json数组字符串
+     *
+     * @return json数组
+     */
     public String build() {
         return node.toString();
+    }
+
+    /**
+     * 转成List
+     *
+     * @param <T> 元素类型
+     * @return {@link  List}
+     */
+    public <T> List<T> toList() {
+        return Json.toList(build());
     }
 }
--- kong-json/src/main/java/io/github/kongweiguang/json/JsonObj.java
+++ kong-json/src/main/java/io/github/kongweiguang/json/JsonObj.java
@@ -3,6 +3,7 @@ package io.github.kongweiguang.json;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -20,20 +21,44 @@ public class JsonObj {
         this.node = node;
     }
 
+    /**
+     * 工厂方法，创建JsonObj
+     *
+     * @return {@link  JsonObj}
+     */
     public static JsonObj of() {
         return new JsonObj();
     }
 
-
+    /**
+     * 工厂方法，创建sonObj
+     *
+     * @param node {@link  ObjectNode}
+     * @return {@link  JsonObj}
+     */
     public static JsonObj of(final ObjectNode node) {
         return new JsonObj(node);
     }
 
+    /**
+     * 添加数据到json对象中
+     *
+     * @param k 健
+     * @param v 值
+     * @return {@link  JsonObj}
+     */
     public JsonObj put(String k, String v) {
         node.put(k, Json.toStr(v));
         return this;
     }
 
+    /**
+     * 添加对象数据到json对象中
+     *
+     * @param k
+     * @param v
+     * @return
+     */
     public JsonObj put(String k, Object v) {
         node.set(k, Json.toNode(Json.toStr(v)));
 
@@ -58,5 +83,9 @@ public class JsonObj {
 
     public String build() {
         return node.toString();
+    }
+
+    public <K, V> Map<K, V> toMap() {
+        return Json.toMap(build());
     }
 }
